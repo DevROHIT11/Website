9178d170d2fa41354d46fb36d5d5463ae2b123c5
Rohit Lahu Bhalekar <165887184+DevROHIT11@users.noreply.github.com>
2025-04-11 18:01:59 +0530

    gallery page updated

--- src/javascript/gallery.js
+++ src/javascript/gallery.js
@@ -3,108 +3,108 @@
     // group-photos
     {
         id: 1,
-        title: "Team Photo 2023",
+        title: "gallery",
         // Each image can have multiple categories in an array
         categories: ["Group-Photos", "all" , "aissms"],
         imageUrl: "src/Assets/gallery/Group Photos/1694715063934-01.jpeg" // Replace with your image path
     },
     {
         id: 2,
-        title: "Traditional Ceremony",
+        title: "gallery",
         categories: ["all", "Group-Photos"],
         imageUrl: "src/Assets/gallery/Group Photos/20240215021143_IMG_2581.jpg" // Replace with your image path
     },
     {
         id: 3,
-        title: "Competition Performance",
+        title: "gallery",
         categories: ["Group-Photos", "all"],
         imageUrl: "src/Assets/gallery/Group Photos/IMG-20240810-WA0018.jpg" // Replace with your image path
     },
     {
         id: 4,
-        title: "New Member Drive",
+        title: "gallery",
         categories: ["Group-Photos", "all" , "tradition"],
         imageUrl: "src/Assets/gallery/Group Photos/IMG-20241012-WA0106.jpg" // Replace with your image path
     },
     {
         id: 5,
-        title: "Championship Trophy",
+        title: "gallery",
         categories: ["Group-Photos", "all" , "aissms"],
         imageUrl: "src/Assets/gallery/Group Photos/IMG-20250220-WA0013.jpg" // Replace with your image path
     },
     {
         id: 6,
-        title: "AISSMS Campus Event",
+        title: "gallery",
         categories: ["Group-Photos", "all"],
         imageUrl: "src/Assets/gallery/Group Photos/IMG_20241020_164551734.jpg" // Replace with your image path
     },
     {
         id: 7,
-        title: "Keystone College Team",
+        title: "gallery",
         categories: ["Group-Photos", "all" , "aissms"],
         imageUrl: "src/Assets/gallery/Group Photos/IMG-20250220-WA0026.jpg" // Replace with your image path
     },
     {
         id: 8,
-        title: "VIT Pune Competition",
+        title: "gallery",
         categories:["Group-Photos", "all" , "winning" , "keystone"],
         imageUrl: "src/Assets/gallery/Group Photos/IMG-20250228-WA0022.jpg" // Replace with your image path
     },
     {
         id: 9,
-        title: "VJTI Annual Event",
+        title: "gallery",
         categories: ["Group-Photos", "all"],
         imageUrl: "src/Assets/gallery/Group Photos/IMG-20250228-WA0046.jpg" // Replace with your image path
     },
     {
         id: 10,
-        title: "Award Ceremony",
+        title: "gallery",
         categories: ["Group-Photos", "all"],
         imageUrl: "src/Assets/gallery/Group Photos/IMG-20250324-WA0007.jpg" // Replace with your image path
     },
     {
         id: 11,
-        title: "Team Huddle",
+        title: "gallery",
         categories: ["Group-Photos", "all" , "vit"],
         imageUrl: "src/Assets/gallery/Group Photos/IMG-20250407-WA0007.jpg" // Replace with your image path
     },
     {
         id: 12,
-        title: "Recruitment Drive",
+        title: "gallery",
         categories: ["Group-Photos", "all" , "winning"],
         imageUrl: "src/Assets/gallery/Group Photos/IMG-20250407-WA0010.jpg" // Replace with your image path
     },
     {
         id: 13,
-        title: "Recruitment Drive",
+        title: "gallery",
         categories: ["Group-Photos", "all" ],
         imageUrl: "src/Assets/gallery/Group Photos/IMG-20250407-WA0012.jpg" // Replace with your image path
     }
     ,
     {
         id: 14,
-        title: "Recruitment Drive",
+        title: "gallery",
         categories: ["Group-Photos", "all"],
         imageUrl: "src/Assets/gallery/Group Photos/IMG-20250407-WA0019.jpg" // Replace with your image path
     }
     ,
     {
         id: 15,
-        title: "Recruitment Drive",
+        title: "gallery",
         categories: ["Group-Photos", "all" , "aissms"],
         imageUrl: "src/Assets/gallery/Group Photos/IMG-20250220-WA0024.jpg" // Replace with your image path
     }
     ,
     {
         id: 16,
-        title: "Recruitment Drive",
+        title: "gallery",
         categories: ["Group-Photos", "all ", "vjti"],
         imageUrl: "src/Assets/gallery/Group Photos/IMG_20241223_205053478_HDR.jpg" // Replace with your image path
     }
     ,
     {
         id: 17,
-        title: "Recruitment Drive",
+        title: "gallery",
         categories: ["Group-Photos", "all" , "vit"],
         imageUrl: "src/Assets/gallery/Group Photos/IMG_20250329_205723.jpg" // Replace with your image path
     },
@@ -113,25 +113,25 @@
   
     {
         id: 18,
-        title: "Recruitment Drive",
+        title: "gallery",
         categories:  ["tradition", "all"],
         imageUrl: "src/Assets/gallery/Tradition/20241012_122314-1.jpeg" // Replace with your image path
     },
     {
         id: 19,
-        title: "Recruitment Drive",
+        title: "gallery",
         categories:  ["tradition", "all"],
         imageUrl: "src/Assets/gallery/Tradition/IMG-20241012-WA0098.jpg" // Replace with your image path
     },
     {
         id: 20,
-        title: "Recruitment Drive",
+        title: "gallery",
         categories:  ["tradition", "all"],
         imageUrl: "src/Assets/gallery/Tradition/20241012_120959.jpeg" // Replace with your image path
     },
     {
         id: 21,
-        title: "Recruitment Drive",
+        title: "gallery",
         categories:  ["tradition", "all"],
         imageUrl: "src/Assets/gallery/Tradition/IMG-20250406-WA0047.jpg" // Replace with your image path
     },
@@ -139,62 +139,62 @@
     // IN action
     {
         id: 22,
-        title: "Recruitment Drive",
+        title: "gallery",
         categories:  ["in-action", "all"],
         imageUrl: "src/Assets/gallery/In action/IMG-20241022-WA0002.jpg" // Replace with your image path
     },
     {
         id: 23,
-        title: "Recruitment Drive",
+        title: "gallery",
         categories:  ["in-action", "all"],
         imageUrl: "src/Assets/gallery/In action/IMG-20250219-WA0002.jpg" // Replace with your image path
     },
     {
         id: 24,
-        title: "Recruitment Drive",
+        title: "gallery",
         categories:  ["in-action", "all"  , "keystone"],
         imageUrl: "src/Assets/gallery/In action/IMG-20250228-WA0059.jpg" // Replace with your image path
     },
     {
         id: 25,
-        title: "Recruitment Drive",
+        title: "gallery",
         categories:  ["in-action", "all" , "vit"],
         imageUrl: "src/Assets/gallery/In action/IMG-20250407-WA0011.jpg" // Replace with your image path
     }
     ,
     {
         id: 26,
-        title: "Recruitment Drive",
+        title: "gallery",
         categories:  ["in-action", "all"],
         imageUrl: "src/Assets/gallery/In action/IMG-20250221-WA0034.jpg" // Replace with your image path
     },
     {
         id: 27,
-        title: "Recruitment Drive",
+        title: "gallery",
         categories:  ["in-action", "all"],
         imageUrl: "src/Assets/gallery/In action/IMG-20250219-WA0003.jpg" // Replace with your image path
     },
     {
         id: 28,
-        title: "Recruitment Drive",
+        title: "gallery",
         categories:  ["in-action", "all" , "vit"],
         imageUrl: "src/Assets/gallery/In action/IMG-20250407-WA0014.jpg" // Replace with your image path
     },
     {
         id: 29,
-        title: "Recruitment Drive",
+        title: "gallery",
         categories:  ["in-action", "all"],
         imageUrl: "src/Assets/gallery/In action/IMG-20250407-WA0015.jpg" // Replace with your image path
     },
     {
         id: 30,
-        title: "Recruitment Drive",
+        title: "gallery",
         categories:  ["in-action", "all" , "vjti"],
         imageUrl: "src/Assets/gallery/In action/IMG_20241223_130421218_HDR_PORTRAIT.jpg" // Replace with your image path
     },
     {
         id: 31,
-        title: "Recruitment Drive",
+        title: "gallery",
         categories:  ["in-action", "all" , "vjti"],
         imageUrl: "src/Assets/gallery/In action/IMG_20241223_194521214_HDR.jpg" // Replace with your image path
     },
@@ -202,13 +202,13 @@
     //Recruitment 
     {
         id: 32,
-        title: "Recruitment Drive",
+        title: "gallery",
         categories:  ["recruitment", "all"],
         imageUrl: "src/Assets/gallery/Recruitment/IMG-20240920-WA0009.jpg" // Replace with your image path
     },
     {
         id: 33,
-        title: "Recruitment Drive",
+        title: "gallery",
         categories:  ["recruitment", "all"],
         imageUrl: "src/Assets/gallery/Recruitment/IMG_20240920_123830.jpg" // Replace with your image path
     }
@@ -217,7 +217,7 @@
     ,
     {
         id: 34,
-        title: "Recruitment Drive",
+        title: "gallery",
         categories:  ["winning", "all"],
         imageUrl: "src/Assets/gallery/Winning/IMG-20250407-WA0003.jpg" // Replace with your image path
     },
@@ -226,7 +226,7 @@
    
     {
         id: 35,
-        title: "Recruitment Drive",
+        title: "gallery",
         categories:  ["aissms", "all"],
         imageUrl: "src/Assets/gallery/AISSMS/IMG-20250220-WA0022.jpg" // Replace with your image path
     },
@@ -234,32 +234,32 @@
 //    KEYSTONE
     {
         id: 36,
-        title: "Recruitment Drive",
+        title: "gallery",
         categories:  ["keystone", "all"],
         imageUrl: "src/Assets/gallery/Keystone College/IMG-20250228-WA0011.jpg" // Replace with your image path
     },
     {
         id: 37,
-        title: "Recruitment Drive",
+        title: "gallery",
         categories:  ["keystone", "all"],
         imageUrl: "src/Assets/gallery/Keystone College/IMG-20250228-WA0040.jpg" // Replace with your image path
     }
     ,
     {
         id: 38,
-        title: "Recruitment Drive",
+        title: "gallery",
         categories:  ["keystone", "all"],
         imageUrl: "src/Assets/gallery/Keystone College/IMG-20250228-WA0046.jpg" // Replace with your image path
     },
     {
         id: 39,
-        title: "Recruitment Drive",
+        title: "gallery",
         categories:  ["keystone", "all"],
         imageUrl: "src/Assets/gallery/Keystone College/IMG-20250228-WA0052.jpg" // Replace with your image path
     },
     {
         id: 40,
-        title: "Recruitment Drive",
+        title: "gallery",
         categories:  ["keystone", "all"],
         imageUrl: "src/Assets/gallery/Keystone College/IMG-20250301-WA0001.jpg" // Replace with your image path
     },
@@ -267,7 +267,7 @@
     // VIT ,PUNE
     {
         id: 41,
-        title: "Recruitment Drive",
+        title: "gallery",
         categories:  ["vit", "all"],
         imageUrl: "src/Assets/gallery/VIT, Pune/IMG-20250407-WA0013.jpg" // Replace with your image path
     },
